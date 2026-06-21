80f7d24f9fdbba1df4f70b4e8924cfec2fe2f429
Anshika <anshikamishra47021@gmail.com>
2026-06-22 02:45:26 +0530

    cat fact api

--- APIs/index.js
+++ APIs/index.js
@@ -28,9 +28,32 @@
 
 //using fetch with asynch await
 
-let url="https://catfact.ninja/fact";
+// let url="https://catfact.ninja/fact";
+// async function getFact(){
+//   let res= await fetch(url);
+//   let data=await res.json();
+//   console.log(data.fact);
+// }
+
+let btn=document.querySelector("button");
+btn.addEventListener("click", async ()=>{
+  let fact= await getFact();
+  console.log(fact);
+  let p=document.querySelector("#result");
+  p.innerText=fact;
+});
+let url=
+"https://catfact.ninja/fact";
+
 async function getFact(){
-  let res= await fetch(url);
-  let data=await res.json();
-  console.log(data.fact);
+  try{
+    let res=await axios.get(url);
+    console.log(res);
+    return res.data.fact;
+    
+  }
+  catch(e){
+    console.log("error", e);
+    return "No fact found";
+  }
 }
